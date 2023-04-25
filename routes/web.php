6891b94e396f65f980925129a025e59a13cc3249
unknown <mutungaeric839@gmail.com>
2023-04-25 20:43:44 +0300

    comments for more understanding

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/product', [ProductController::class, 'index']);//when the url is passed it calls the product controller parses the function index in the class
